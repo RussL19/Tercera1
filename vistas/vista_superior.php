5fb860296d5e1022cc02a2a713af431b041546a7
Russell Lopez Rodriguez <russelllopez2003@gmail.com>
2025-10-13 10:07:02 -0600

    Creacion de proyecto

---
+++ vistas/vista_superior.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<HTML lang="SPANISH">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="Tercera 1" content="Proyecto Universitario">
+    
+    <title>Tercera 1</title>
+
+    <link rel="stylesheet" href= "./css.main.css">
+<head>
